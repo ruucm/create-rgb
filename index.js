6b389027bfafde6c2699c69b5d7eb4ed11b169f6
ruucm <ruucm.a@gmail.com>
2018-08-10 05:43:48 +0900

    update details

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-console.log('yay gist~')
 var shell = require('shelljs')
 var readlineSync = require('readline-sync')
 
@@ -9,49 +8,60 @@ if (!shell.which('git')) {
   shell.exit(1)
 }
 
-// Wait for user's response.
+// App Name
 var appName = readlineSync.question('May I have your App name? ')
-console.log('Hi ' + appName + '!')
+console.log('Hello ' + appName + '!')
 
-// Handle the secret text (e.g. password).
+// Git repo url
 var gitUrl = readlineSync.question('What is your git repo url? ', {
   hideEchoBack: true, // The typed text on screen is hidden by `*` (default).
 })
-// console.log('Oh, ' + appName + ' loves ' + gitUrl + '!')
 
-// shell.read('-p', 'App Name: ', name)
 shell.echo('make dir at : ' + appName)
 shell.mkdir(appName)
 shell.cd(appName)
 
-// Run external tool synchronously
-if (
-  shell.exec('git clone https://github.com/ruucm/react-gui-builder.git .')
-    .code !== 0
-) {
-  shell.echo('Error: Git clone failed')
-  shell.exit(1)
-} else {
-  shell.rm('-rf', '.git')
-
-  if (shell.exec('git init').code !== 0) {
-    shell.echo('Error: Git init failed')
-    shell.exit(1)
-  }
-
-  if (shell.exec('git remote add origin ' + gitUrl).code !== 0) {
-    shell.echo('Error: Git remote add origin failed')
-    shell.exit(1)
-  }
-
+if (gitUrl) {
+  // Run external tool synchronously
   if (
-    shell.exec(
-      'git add --all && git commit -m "Init 🎉" && git push origin master'
-    ).code !== 0
+    shell.exec('git clone https://github.com/ruucm/react-gui-builder.git .')
+      .code !== 0
   ) {
-    shell.echo('Error: Git push failed')
+    shell.echo('Error: Git clone failed')
     shell.exit(1)
+  } else {
+    shell.rm('-rf', '.git')
+
+    if (shell.exec('git init').code !== 0) {
+      shell.echo('Error: Git init failed')
+      shell.exit(1)
+    }
+
+    if (shell.exec('git remote add origin ' + gitUrl).code !== 0) {
+      shell.echo('Error: Git remote add origin failed')
+      shell.exit(1)
+    }
+
+    if (
+      shell.exec(
+        'git add --all && git commit -m "Init 🎉" && git push origin master'
+      ).code !== 0
+    ) {
+      shell.echo('Error: Git push failed')
+      shell.exit(1)
+    }
+    // npm install
+    if (shell.exec('npm install').code !== 0) {
+      shell.echo('Error: npm install failed')
+      shell.exit(1)
+    } else {
+      if (shell.exec('npm run dev').code !== 0) {
+        shell.echo('Error: npm run dev failed')
+        shell.exit(1)
+      }
+    }
   }
+} else {
   // npm install
   if (shell.exec('npm install').code !== 0) {
     shell.echo('Error: npm install failed')
