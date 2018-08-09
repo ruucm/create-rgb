249a9d13d49875b3a5a92ea7907b0e113c142155
ruucm <ruucm.a@gmail.com>
2018-08-10 04:46:37 +0900

    up

--- index.js
+++ index.js
@@ -57,11 +57,11 @@
 
 // require('./createReactGuiBuilder')
 
-const exec = require('child_process').exec
-var yourscript = exec('sh ./create-rgb.sh', (error, stdout, stderr) => {
-  console.log(`${stdout}`)
-  console.log(`${stderr}`)
-  if (error !== null) {
-    console.log(`exec error: ${error}`)
-  }
-})
+// const exec = require('child_process').exec
+// var yourscript = exec('sh ./create-rgb.sh', (error, stdout, stderr) => {
+//   console.log(`${stdout}`)
+//   console.log(`${stderr}`)
+//   if (error !== null) {
+//     console.log(`exec error: ${error}`)
+//   }
+// })
